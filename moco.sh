992b8888fa9e44714ae6e3cedc793655641d8205
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-01-24 18:49:43 -0800

    716 anat

--- moco.sh
+++ moco.sh
@@ -13,5 +13,4 @@ module use /home/groups/trc/modules
 module load antspy/0.2.2
 
 python3 /home/users/asmart/projects/justmoco/motion_correction.py 
-/oak/stanford/groups/trc/data/Ashley2/imports/20210709/fly1_40s-006
-
+/oak/stanford/groups/trc/data/Ashley2/imports/20210716_2/fly1_20s_anat-010
