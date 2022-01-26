49107edbf758585084dce31ad1fcdb1a21814cba
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2022-01-25 16:44:58 -0800

    up cpus to 6

--- moco.sh
+++ moco.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=trc
 #SBATCH --time=96:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=6
 #SBATCH --output=/home/users/asmart/projects/justmoco/logs/%x.%j.out
 #SBATCH --open-mode=append
 
